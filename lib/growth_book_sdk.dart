a19112c7ce3cdc0e383387640889f14c298fe520
Dhruvin <dhruvin.vainsh02@gmail.com>
2022-04-18 20:29:09 +0530

    Added Stream Support

--- lib/growth_book_sdk.dart
+++ lib/growth_book_sdk.dart
@@ -1,4 +1,4 @@
-library growthbook_sdk;
+library growth_book_sdk;
 
 export '../src/growth_book_sdk.dart';
 export '../src/model/model.dart';
---
+++ lib/src/Helper/state_helper.dart
@@ -0,0 +1,13 @@
+/// It will be helpful incase we want to show status of our sdk
+/// builder.
+
+enum StateHelper {
+  /// While fetching.
+  loading,
+
+  /// While fetched.
+  fetched,
+
+  /// While error ocurred.
+  error
+}
