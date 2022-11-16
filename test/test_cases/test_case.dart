cfcb68eb180729d5a439feebe829cbdd873cc000
Dhruvin <dhruvin.vainsh02@gmail.com>
2022-11-17 00:51:43 +0530

    fix:string comparision

--- test/test_cases/test_case.dart
+++ test/test_cases/test_case.dart
@@ -26,45 +26,7 @@ const String gbTestCases = r'''
     },
     false
   ],
-  [
-    "$gt    /$lt     strings - fail $lt    ",
-    {
-      "word": {
-        "$gt": "alphabet",
-        "$lt": "zebra"
-      }
-    },
-    {
-      "word": "zebra"
-    },
-    false
-  ],
-  [
-    "$gt    /$lt     strings - pass",
-    {
-      "word": {
-        "$gt": "alphabet",
-        "$lt": "zebra"
-      }
-    },
-    {
-      "word": "always"
-    },
-    false
-  ],
-  [
-    "$gt    /$lt     strings - fail uppercase",
-    {
-      "word": {
-        "$gt": "alphabet",
-        "$lt": "zebra"
-      }
-    },
-    {
-      "word": "AZL"
-    },
-    false
-  ],
+
         [
           "$not     - fail",
           {
